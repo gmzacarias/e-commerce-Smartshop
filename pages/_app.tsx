b80be3b11cfbaea42bac2cab534d8487c33aa2ce
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-12-28 20:49:54 -0300

    fix app ,se elimino imports no utilizados

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,12 @@
+import { Suspense } from "react";
 import type { AppProps } from "next/app"
-import { PrivateRoute } from "@/utils/private-routes"
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider, ThemeConsumer } from "@/utils/darkMode/themeContext";
 import { Toaster } from "sonner"
 import "styles/base.css"
-import { Suspense } from "react";
-
-import { useTheme, ThemeProvider, ThemeConsumer } from "@/utils/darkMode/themeContext";
-
-
-
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { isDarkMode } = useTheme();
+
   return (
     <Suspense>
       <RecoilRoot>
@@ -20,9 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Toaster expand={true} richColors position="bottom-right" />
             <link rel="icon" href="/favicon.png" sizes="any" />
             <Component {...pageProps} />
-
           </ThemeConsumer>
-
         </ThemeProvider>
       </RecoilRoot>
     </Suspense>
