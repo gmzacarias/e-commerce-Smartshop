dd3aa07a060d53551faef87a5a07283c3ce92a2a
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-01-04 17:02:01 -0300

    fix imports component Home & create styles

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,26 +1,17 @@
-import styled from "styled-components"
+import { Title } from "@/ui/typography"
+import { Search } from "@/components/search"
 import { FeaturedProducts } from "@/components/featuredProducts"
-import {Title,SubTitle } from "@/ui/typography"
-import { Search} from "../search"
-import { PurchaseByBrand } from "../purchaseByBrand"
-
-const HomeBody = styled.div`
-display:flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: center;
-/* gap:10px; */
-`
+import { PurchaseByBrand } from "@/components/purchaseByBrand"
+import {HomeBody} from "./styles"
 
 export function Home() {
 
     return (
         <HomeBody>
-            <Title>Bienvenido a Smartshop</Title>            
-            <Search/>
+            <Title>Bienvenido a Smartshop</Title>
+            <Search />
             <FeaturedProducts></FeaturedProducts>
-            <PurchaseByBrand/>
+            <PurchaseByBrand />
         </HomeBody>
-
     )
 }
