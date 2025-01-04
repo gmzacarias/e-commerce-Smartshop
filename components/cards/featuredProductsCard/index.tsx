4b4595c56d0b12f7f4412a5fb743265852efcc82
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-01-04 17:36:47 -0300

    fix imports en featuredProducts & se movio el codigo de FeaturedProductsCard a un componente propio

---
+++ components/cards/featuredProductsCard/index.tsx
@@ -0,0 +1,45 @@
+import { ProductsContainer, ProductCard, PhotoCard, ButtonCard } from "@/components/featuredProducts/styles"
+import { useProductsList } from "@/lib/hooks"
+import router from "next/router"
+import Skeleton from "react-loading-skeleton"
+
+
+export function FeaturedProductsCard() {
+    const data = useProductsList()
+    console.log("data destacada", data)
+
+
+    function handleRedirect(id) {
+        confirm(`redirigir al producto ${id}`)
+        router.push(`/product/${id}`)
+
+    }
+
+    return (
+        <ProductsContainer>
+            {data ? (
+                data.map((item) => (
+                    <ProductCard key={item.id}>
+                        <PhotoCard src={item.photo} />
+                        <h3 style={{ fontSize: 14, fontWeight: "normal" }}>{`${item.brand} ${item.model} ${item.storage}`}</h3>
+                        <h3 style={{ fontSize: 16, fontWeight: "bold" }}>{`$${item.price}`}</h3>
+                        <ButtonCard onClick={() => handleRedirect(item.id)}>¡Lo quiero!</ButtonCard>
+                    </ProductCard>
+
+                ))
+
+            ) : (
+                Array.from({ length: 3 }).map((_, index) => (
+                    <ProductCard key={index}>
+                        <Skeleton width={200} height={180} borderRadius={5} baseColor="#fff" />
+                        <Skeleton width={150} height={27} borderRadius={5} baseColor="#fff" />
+                        <Skeleton width={80} height={27} borderRadius={5} baseColor="#fff" />
+                        <Skeleton width={158} height={44} borderRadius={4} baseColor="#fff" />
+                    </ProductCard>
+                ))
+            )}
+        </ProductsContainer>
+    )
+
+
+}
--- components/featuredProducts/index.tsx
+++ components/featuredProducts/index.tsx
@@ -1,51 +1,12 @@
-
 import { SubTitle } from "@/ui/typography"
-import { useProductsList } from "@/lib/hooks"
-import Skeleton from "react-loading-skeleton"
-import router from "next/router"
-import { FeaturedProductsSection, ProductsContainer, ProductCard, PhotoCard, ButtonCard } from "./styles"
-
-
-
-
-
+import { FeaturedProductsCard } from "@/components/cards/featuredProductsCard"
+import { FeaturedProductsSection } from "./styles"
 export function FeaturedProducts() {
-    const data = useProductsList()
-    console.log("data destacada", data)
-
-
-function handleRedirect(id){
-    confirm(`redirigir al producto ${id}`)
-    router.push(`/product/${id}`)
-    
-}
 
     return (
         <FeaturedProductsSection>
             <SubTitle>Productos destacados</SubTitle>
-            <ProductsContainer>
-                {data ? (
-                    data.map((item) => (
-                        <ProductCard key={item.id}>
-                            <PhotoCard src={item.photo} />
-                            <h3 style={{ fontSize: 14, fontWeight: "normal" }}>{`${item.brand} ${item.model} ${item.storage}`}</h3>
-                            <h3 style={{ fontSize: 16, fontWeight: "bold" }}>{`$${item.price}`}</h3>
-                            <ButtonCard onClick={()=>handleRedirect(item.id)}>¡Lo quiero!</ButtonCard>
-                        </ProductCard>
-                        
-                    ))
-              
-                ) : (
-                    Array.from({ length:3}).map((_, index) => (
-                        <ProductCard key={index}>
-                            <Skeleton width={200} height={180} borderRadius={5} baseColor="#fff" />
-                            <Skeleton width={150} height={27} borderRadius={5} baseColor="#fff" />
-                            <Skeleton width={80} height={27} borderRadius={5} baseColor="#fff" />
-                            <Skeleton width={158} height={44} borderRadius={4} baseColor="#fff" />
-                        </ProductCard>
-                    ))
-                )}
-            </ProductsContainer>
+            <FeaturedProductsCard />
         </FeaturedProductsSection>
     )
 }
