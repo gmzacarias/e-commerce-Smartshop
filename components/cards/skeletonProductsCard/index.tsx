cbf3edb7ff6f82f744247b7ccf1ab730b548cbea
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-01-07 17:03:02 -0300

    delete import sin usar

--- components/cards/skeletonProductsCard/index.tsx
+++ components/cards/skeletonProductsCard/index.tsx
@@ -1,28 +1,27 @@
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import { SkeletonContainer,SkeletonImage, SkeletonText,SkeletonButton } from "./styles"
+import { SkeletonTheme } from "react-loading-skeleton"
 import { useTheme } from "@/utils/darkMode/themeContext"
+import { SkeletonContainer, SkeletonImage, SkeletonText, SkeletonButton } from "./styles"
 
 type LengthProps = {
     length: number
 }
 
 export function SkeletonProductsCard({ length }: LengthProps) {
- const {isDarkMode}=useTheme()
+    const { isDarkMode } = useTheme()
     const baseColor = isDarkMode ? "#000" : "#d0d0d0"
-    const highlightColor=isDarkMode ? "#292728" : "#f5f5f5"
-  
+    const highlightColor = isDarkMode ? "#292728" : "#f5f5f5"
+
     return (
-      <SkeletonTheme borderRadius={5} baseColor={baseColor} highlightColor={highlightColor}>
-        {Array.from({ length }).map((_, index) => (
-          <SkeletonContainer key={index}>
-            <SkeletonImage />
-            <SkeletonText />
-            <SkeletonText />
-            <SkeletonText />
-            <SkeletonButton />
-          </SkeletonContainer>
-        ))}
-      </SkeletonTheme>
+        <SkeletonTheme borderRadius={5} baseColor={baseColor} highlightColor={highlightColor}>
+            {Array.from({ length }).map((_, index) => (
+                <SkeletonContainer key={index}>
+                    <SkeletonImage />
+                    <SkeletonText />
+                    <SkeletonText />
+                    <SkeletonText />
+                    <SkeletonButton />
+                </SkeletonContainer>
+            ))}
+        </SkeletonTheme>
     );
-  }
-  
+}
