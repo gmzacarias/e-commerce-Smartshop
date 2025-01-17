0d618cf55f30050b53b3d2c12661f9dcbfd34ca4
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-01-16 23:45:50 -0300

    fix Loader component

---
+++ ui/loader/index.tsx
@@ -0,0 +1,9 @@
+import { Loader, LoaderContainer } from "./styles";
+
+export function LoaderApp (){
+    return (
+        <LoaderContainer>
+            <Loader/>
+        </LoaderContainer>
+    )
+}
