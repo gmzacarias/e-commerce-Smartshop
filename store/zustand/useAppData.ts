efbdfffa6d69751c8d310112fcfad0d1456688c0
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-03-21 01:42:50 -0300

    Fix - se corrigio useAppDataStore, para que los metodos setters sean opcionales,tambien se modifico el nombre del store para ser guardado en localStorage

--- store/zustand/useAppData.ts
+++ store/zustand/useAppData.ts
@@ -2,15 +2,15 @@ import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
 interface IAppData {
-    email?: string,
-    isLogged?: boolean,
-    query?: string,
+    email: string,
+    isLogged: boolean,
+    query: string,
     updateEmail?: (newEmail: string) => void,
     updateLogin?: (isLogged: boolean) => void,
     updateQuery?: (query: string) => void
 }
 
-const useAppDataStore = create<IAppData>()(
+export const useAppDataStore = create<IAppData>()(
     persist(
         (set) => ({
             email: "",
@@ -20,6 +20,6 @@ const useAppDataStore = create<IAppData>()(
             updateLogin: (isLogged) => set({ isLogged }),
             updateQuery: (query) => set({ query }),
         }),
-        { name: "useAppDataStorage" },
+        { name: "app-data" },
     ),
 )
