3aac3b5611325b6eea1b9c50519dbec75307097f
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-21 19:44:12 -0300

    Fix - se elimino el setTimeout

--- utils/privateRoutes.tsx
+++ utils/privateRoutes.tsx
@@ -8,17 +8,15 @@ import { LoaderApp } from '@/ui/loader';
 type RoutesProps = {
     children: React.ReactNode
 }
+
 function PrivateRoutes({ children }: RoutesProps) {
     const [isLoading, setIsLoading] = useState(true)
-    const {isLogged}=useAppDataStore()
+    const { isLogged } = useAppDataStore()
     const router = useRouter()
 
     useEffect(() => {
         if (!isLogged) {
-           
-           setTimeout(() => {
-               router.replace("/")
-           }, 10000);
+                router.replace("/")
             return
         }
         else {
