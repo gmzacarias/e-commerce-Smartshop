450fad7d438b13c94d9a1b777eff7f184bbf1c37
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-21 17:27:47 -0300

    Fix - se creo el component OTPForm para manejar la logica del form en un component individual.

---
+++ features/auth/components/otpForm/index.tsx
@@ -0,0 +1,62 @@
+import { Controller } from "react-hook-form";
+import { useLogin } from "../../hooks/useLogin";
+import { Label, Small } from "@/ui/typography";
+import { RefOTPInput } from "@/ui/inputs";
+import { FormButton } from "@/ui/buttons";
+import { OTPContainer } from "./styles";
+
+export function OTPForm(){
+ const {handleCodeSubmit,handleCodeForm,onErrorCode,controlCode,otpRef,handleKeyUp,resendCode}=useLogin()
+    return (
+        <>
+         <form onSubmit={handleCodeSubmit(handleCodeForm, onErrorCode)}>
+                            <Label>
+                                Codigo
+                                <Small>
+                                    Enviamos un código de verificación a email
+                                </Small>
+                                <OTPContainer>
+                                    {Array.from({ length: 5 }).map((_, index) => (
+                                        <Controller
+                                            key={index}
+                                            control={controlCode}
+                                            name={`otp.${index}`}
+                                            rules={{
+                                                required: "ingrese solo numeros",
+                                                pattern: {
+                                                    value: /^\d$/,
+                                                    message: "Formato de codigo inválido",
+                                                },
+                                            }
+                                            }
+                                            render={({ field: { onChange, value } }) => (
+                                                <RefOTPInput
+                                                    type="text"
+                                                    ref={(element) => (otpRef.current[index] = element)}
+                                                    placeholder="-"
+                                                    maxLength={1}
+                                                    onChange={onChange}
+                                                    onKeyUp={() => handleKeyUp(index)}
+                                                    value={value}
+                                                    required
+                                                />
+                                            )
+                                            }
+                                        />
+                                    ))
+                                    }
+                                </OTPContainer>
+                            </Label>
+                            <FormButton title="iniciar sesion">
+                                INGRESAR
+                            </FormButton>
+                        </form>
+                        <Small>
+                            ¿No recibiste el código?
+                        </Small>
+                        <button onClick={resendCode} style={{ background: "none", border: "none", cursor: "pointer" }}>
+                            reenviar codigo
+                        </button>
+        </>
+    )
+}
