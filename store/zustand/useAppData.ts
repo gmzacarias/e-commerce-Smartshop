870c52a8335d0b2e61b5d579ae08e221af7f4d96
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-21 17:56:27 -0300

    Fix - se añadio la funcion reset para resetear el estado de la app

--- store/zustand/useAppData.ts
+++ store/zustand/useAppData.ts
@@ -1,24 +1,30 @@
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
-interface IAppData {
+interface AppState {
     email: string,
     isLogged: boolean,
     query: string,
     updateEmail?: (newEmail: string) => void,
     updateLogin?: (isLogged: boolean) => void,
-    updateQuery?: (query: string) => void
+    updateQuery?: (query: string) => void,
+    reset?: () => void
 }
 
-export const useAppDataStore = create<IAppData>()(
+const initialState: AppState = {
+    email: "",
+    isLogged: false,
+    query: "",
+}
+
+export const useAppDataStore = create<AppState>()(
     persist(
         (set) => ({
-            email: "",
-            isLogged: false,
-            query: "",
+            ...initialState,
             updateEmail: (email) => set({ email }),
             updateLogin: (isLogged) => set({ isLogged }),
             updateQuery: (query) => set({ query }),
+            reset: () => set(initialState)
         }),
         { name: "app-data" },
     ),
