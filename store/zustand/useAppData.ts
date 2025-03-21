6e6cfe02087cc541860be33f7bfe74613220cbe5
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-03-21 00:22:12 -0300

    Fix - se modifico la logica para guardar los datos en localStorage

--- store/zustand/useAppData.ts
+++ store/zustand/useAppData.ts
@@ -1,21 +1,25 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 interface IAppData {
-    email: string,
-    isLogged: boolean,
-    query: string,
-    updateEmail: (newEmail: string) => void,
+    email?: string,
+    isLogged?: boolean,
+    query?: string,
+    updateEmail?: (newEmail: string) => void,
     updateLogin?: (isLogged: boolean) => void,
     updateQuery?: (query: string) => void
 }
 
-export const useAppDataStore = create<IAppData>((set) => ({
-    email: "",
-    isLogged: false,
-    query: "",
-    updateEmail: (email) => set({ email }),
-    updateLogin: (isLogged) => set({ isLogged }),
-    updateQuery:(query)=>set({query})
-}
-)
+const useAppDataStore = create<IAppData>()(
+    persist(
+        (set) => ({
+            email: "",
+            isLogged: false,
+            query: "",
+            updateEmail: (email) => set({ email }),
+            updateLogin: (isLogged) => set({ isLogged }),
+            updateQuery: (query) => set({ query }),
+        }),
+        { name: "useAppDataStorage" },
+    ),
 )
