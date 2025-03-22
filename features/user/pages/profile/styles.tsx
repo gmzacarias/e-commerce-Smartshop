20ba0904e84865bc9698a657b890dfebf82109fc
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-22 18:38:23 -0300

    Fix - se modifico la logica de Profile  en la forma de crear el component ,ademas se eliminaron los components de estilos sin usar

--- features/user/pages/profile/styles.tsx
+++ features/user/pages/profile/styles.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components"
 
-
 export const ProfileBody = styled.div`
   width: 100%;
   height: 100vh;
@@ -32,41 +31,7 @@ export const PageContainer = styled.section`
   @media(min-width:769px){
     padding: 70px;
     gap: 10px;
-  }
-  
+  }  
 `
 
-export const ErrorContainer = styled.div`
-  background-color: ${({ theme }) => theme.mode === "dark" ? "#ff4444" : "#f8d7da"};
-  border: 1px solid ${({ theme }) => theme.mode === "dark" ? "#ff0000" : "#f5c6cb"};
-  border-radius: 5px;
-  margin: 20px 0;
-  padding: 15px;
-  width: 100%;
-  max-width:fit-content;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-`
 
-export const ProfileDataContainer = styled.div`
-  background-color:${({ theme }) => theme.mode === "dark" ? "#121212" : "#f2f4f5"};
-  border-radius: 20px;
-  box-shadow: 0 2px 6px 1px ${({ theme }) => theme.mode === "dark" ? "#0a0b0c" : "#cbd0d5"};
-  width: 100%;
-  max-width: 500px;
-  height:fit-content;
-  padding: 30px;
-  display:flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  gap:10px;    
-`
-export const ButtonsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction:column ;
-  align-items: center;
-`
