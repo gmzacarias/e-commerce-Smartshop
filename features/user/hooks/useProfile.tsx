d4922091fec41d56351e02c5f83b7631fdae8501
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-22 16:57:38 -0300

    Fix - se modifico la logica de useProfile para poder guardar los datos en el estado.

---
+++ features/user/hooks/useProfile.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import { useMe } from "@/hooks/swr/useMe"
+import { useUserData } from "@/store/zustand/useUserData"
+import { handleNavigation } from "@/utils/handleNavigation"
+
+export function useProfile() {
+    const { data, isLoading, error, isError } = useMe()
+    const { updateData } = useUserData()
+
+    useEffect(() => {
+        if (data) {
+            updateData!({
+                email: data.email,
+                userName: data.userName,
+                address: data.address,
+                phoneNumber: data.phonumber
+            })
+        }
+    }, [data])
+
+    return { data, isLoading, error, isError, handleNavigation }
+}
