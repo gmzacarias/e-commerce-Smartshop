d6675278c409f5df57d57def07d72f1cf4213b1c
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-22 16:31:03 -0300

    Fix - se creo useUserData para controlar el estado de los datos del usuario almacenados en local storage

---
+++ store/zustand/useUserData.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+interface UserDataState {
+    email: string,
+    userName: string,
+    address: string,
+    phoneNumber: number,
+    updateData?: (partialData?: Partial<UserDataState>) => void
+    resetData?: () => void
+}
+const initialState: UserDataState = {
+    email: "",
+    userName: "",
+    address: "",
+    phoneNumber: 0
+}
+
+
+export const useUserData = create<UserDataState>()(
+    persist(
+        (set, get) => ({
+            ...initialState,
+            updateData: (partialData = {}) => set({ ...get(), ...partialData }),
+            resetData: () => { set(initialState) }
+        }),
+        { name: "user-data" }
+    )
+)
