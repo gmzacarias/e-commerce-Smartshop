7d6f17737c1cbdc24951feb275a0fcf76ed03ca8
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-24 17:46:24 -0300

    refactor - se corrigio la logica al renderizar Login,modificando los components que contienen el form

--- features/auth/pages/login/index.tsx
+++ features/auth/pages/login/index.tsx
@@ -1,22 +1,14 @@
 import { useLogin } from "@/features/auth/hooks/useLogin";
+import { PagesContainer } from "@/ui/containers/pagesContainer";
 import { EmailForm } from "@/features/auth/components/emailForm";
 import { OTPForm } from "@/features/auth/components/otpForm";
-import { SubTitle } from "ui/typography"
-import { BrandPagesIcon } from "@/ui/icons";
-import { LoginBody, PageContainer, FormContainer } from "./styles";
 
 export function Login() {
     const { currentEmail } = useLogin()
-
     return (
-        <LoginBody>
-            <PageContainer>
-                <BrandPagesIcon />
-                <SubTitle>Ingresa a tu cuenta</SubTitle>
-            </PageContainer>
-            <FormContainer>
-                {!currentEmail ? (<EmailForm />) : (<OTPForm />)}
-            </FormContainer>
-        </LoginBody >
+        <PagesContainer isAuth={true} subTitleText="Ingrese tu cuenta">
+            {!currentEmail ? (<EmailForm />) : (<OTPForm />)}
+        </PagesContainer>
+
     )
 }
