5f2f55719170f95239ce924e7da0b3f9c216862b
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-25 20:25:22 -0300

    refactor - se corrigio la logica para obtener los datos de las ordenes

--- hooks/swr/useOrders.ts
+++ hooks/swr/useOrders.ts
@@ -4,7 +4,7 @@ import { fetchApi } from "@/api/fetchApi"
 export function useOrders() {
     const { data, error, isLoading } = useSWR("/order/myorders", fetchApi as any)
     return {
-        data,
+        data:data?.data,
         isLoading,
         isError: !!error,
         error
