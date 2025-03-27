8b5ef220373bb8ec8a79e14c0e03c157582c0de7
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-27 19:03:15 -0300

    feat - se creo el component MyOrdersData para poder manejar la logica de renderizar los datos segun lo que se reciba de useMyOrders

---
+++ features/user/components/myOrdersData/index.tsx
@@ -0,0 +1,29 @@
+import { useMyOrders } from "@/features/user/hooks/useMyOrders";
+import { ErrorDataCard } from "@/ui/cards/errorDataCard";
+import { OrdersCard } from "@/ui/cards/ordersCard";
+import { OrderDataCard } from "@/ui/cards/orderDataCard";
+
+export function MyOrdersData() {
+    const { data, isLoading, isError, error } = useMyOrders()
+    if (isLoading) {
+        return (
+            <div>
+                cargando....
+            </div>
+        )
+    } else if (isError || data === undefined) {
+        return <ErrorDataCard text="Hubo un error al cargar las ordenes" error={error!.message} />
+    }
+
+    return (
+        <>
+            {data.map((item) => (
+                <OrdersCard key={item.id} data={data}>
+                    {(product) => (
+                        <OrderDataCard key={product.productId} product={[product]} status={item.status} />
+                    )}
+                </OrdersCard>
+            ))}
+        </>
+    )
+}
