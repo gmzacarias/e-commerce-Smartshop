07d7a8b4c7aa0105ecb792863709ef41a0413c3c
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-27 19:09:12 -0300

    refactor - se corrigio la logica de useOrdersById para obtener los datos que se obtienen de data

---
+++ hooks/swr/useOrdersById.ts
@@ -0,0 +1,12 @@
+import useSWR from "swr"
+import { fetchApi } from "@/api/fetchApi"
+
+export function useOrdersById(id: string) {
+    const { data, error, isLoading } = useSWR(`/order/myorders/${id}`, fetchApi as any)
+    return{
+        data:data?.data,
+        isLoading,
+        isError:!!error,
+        error
+    }
+}
