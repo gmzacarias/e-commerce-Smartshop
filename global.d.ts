7cb8847d15bf4c456c4d4f81112d85c395543903
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-26 23:22:50 -0300

    chore : se movio el type PageContainerProps al archivo global para estructurar mejor el codigo

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,12 @@
 import { ReactNode } from "react"
 
 declare global {
+    type PagesContainerProps = {
+        $isAuth: boolean
+        subTitleText: string,
+        children: ReactNode
+    }
+
     /*Auth*/
     interface GetTokenResponse {
         email: string,
@@ -58,7 +64,7 @@ declare global {
     }
 
     interface UseOrderData {
-        data:OrdersData[],
+        data: OrdersData[],
         isLoading: boolean,
         isError: boolean,
         error: Error | null
--- ui/containers/pagesContainer/index.tsx
+++ ui/containers/pagesContainer/index.tsx
@@ -1,16 +1,8 @@
-import { ReactNode } from "react";
-import { AuthFormContainer, ContentContainer, PageContainer } from "./styles";
-import { SubTitle } from "@/ui/typography";
 import { BrandPagesIcon } from "@/ui/icons";
-
-type PagesContainerProps = {
-    $isAuth: boolean
-    subTitleText: string,
-    children: ReactNode
-}
+import { SubTitle } from "@/ui/typography";
+import { AuthFormContainer, ContentContainer, PageContainer } from "./styles";
 
 export function PagesContainer({ $isAuth, subTitleText, children }: PagesContainerProps) {
-
     return (
         <PageContainer $isAuth={$isAuth}>
             <ContentContainer $isAuth={$isAuth}>
