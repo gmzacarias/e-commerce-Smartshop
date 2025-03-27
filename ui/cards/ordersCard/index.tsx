45afc1f18a6bab331f9d4a7d8327a87049f72a2c
gastonmzacarias <gastonmzacarias@gmail.com>
2025-03-27 00:01:51 -0300

    feat - se crearon los components OrdersCardContainer y OrdersCardContent con sus respectivos styles para mejorar la estructura del codigo

--- ui/cards/ordersCard/index.tsx
+++ ui/cards/ordersCard/index.tsx
@@ -1,17 +1,21 @@
+import { OrdersCardContainer, OrdersCardContent } from "./styles";
+
 export function OrdersCard({ data, children }: OrdersCardProps) {
 
     return (
         <>
             {data.map((item) => (
-                <div key={item.id}>
-                    <h4>{item.created}</h4>
-                    <h5>{item.status === "pending" ? "Pago pendiente" : ""}</h5>
+                <OrdersCardContainer key={item.id}>
+                    <OrdersCardContent>
+                        <h4>{item.created}</h4>
+                        <h5>{item.status === "pending" ? "Pago pendiente" : "Pago Exitoso"}</h5>
+                    </OrdersCardContent>
                     {item.products?.map((product) => (
                         <div key={product.productId}>
                             {children(product, item.status)}
                         </div>
                     ))}
-                </div>
+                </OrdersCardContainer>
             ))}
         </>
     )
---
+++ ui/cards/ordersCard/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const OrdersCardContainer = styled.article`
+background-color:#eaeaea;
+border: solid 1px #979797;
+border-radius: 10px;
+width:100%;
+margin-bottom: 10px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`
+export const OrdersCardContent = styled.div`
+padding:5px 16px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+`
